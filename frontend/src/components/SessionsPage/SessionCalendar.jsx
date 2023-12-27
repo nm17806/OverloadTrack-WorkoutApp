e1870f68e6e9dd15aa46b6f158b10f8e0775fa53
Nick McCauley <busted_106@hotmail.com>
2023-12-27 11:12:45 +1100

    updated some styles

--- frontend/src/components/SessionsPage/SessionCalendar.jsx
+++ frontend/src/components/SessionsPage/SessionCalendar.jsx
@@ -1,6 +1,6 @@
 import { eachDayOfInterval, endOfMonth, format, getDay, isToday, startOfMonth, subMonths, addMonths } from "date-fns";
-import { useMemo, useState } from "react";
-import "./EventCalendar.css";
+import React, { useMemo, useState } from "react";
+import "./SessionCalendar.css";
 import ConvertDate from "../Shared/ConvertDate";
 
 const WEEKDAYS = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
@@ -21,14 +21,12 @@ const SessionCalendar = ({ sessions }) => {
 
   const dateSessionMap = useMemo(() => {
     if (sessions) {
-      console.log(sessions);
       const map = {};
       // eslint-disable-next-line no-unused-vars
       Object.entries(sessions).forEach(([recordId, sessions]) => {
         const date = ConvertDate(sessions[0].workout_date);
         map[date] = { template_name: sessions[0].template_name, record_id: recordId };
       });
-      console.log(map);
       return map;
     } else {
       return {};
@@ -48,45 +46,49 @@ const SessionCalendar = ({ sessions }) => {
   };
 
   return (
-    <div className="event-calendar-container">
-      <div className="header">
-        <h2 className="text-center">{format(currentDate, "MMMM yyyy")}</h2>
-        <button className="nav-button" onClick={goToPreviousMonth}>
-          &lt;
-        </button>
-        <button className="nav-button" onClick={goToNextMonth}>
-          &gt;
-        </button>
-      </div>
-      <div className="grid">
-        {WEEKDAYS.map((day) => (
-          <div key={day} className="day-label font-bold text-center">
-            {day}
+    <React.Fragment>
+      <div className="event-calendar-container">
+        <div className="header">
+          <h2 className="text-center">{format(currentDate, "MMMM yyyy")}</h2>
+          <div className="button-container">
+            <button className="nav-button" onClick={goToPreviousMonth}>
+              &lt;
+            </button>
+            <button className="nav-button" onClick={goToNextMonth}>
+              &gt;
+            </button>
           </div>
-        ))}
-        {Array.from({ length: startingDayIndex }).map((_, index) => (
-          <div key={`empty-${index}`} className="empty-day border text-center" />
-        ))}
-        {daysInMonth.map((day, index) => {
-          const formattedDate = ConvertDate(day);
-          const templateName = dateSessionMap[formattedDate];
-          return (
-            <div key={index} className={`day-cell border text-center ${isToday(day) ? "today-cell" : ""}`}>
-              {format(day, "d")}
-              {templateName && (
-                <div
-                  onClick={() => handleSessionSelection(templateName.record_id)}
-                  className="event-cell border bg-secondary text-light"
-                >
-                  {templateName.template_name}
-                </div>
-              )}
-              {!templateName && <div>{templateName ? templateName.template_name : "\u00A0"}</div>}
+        </div>
+        <div className="grid">
+          {WEEKDAYS.map((day) => (
+            <div key={day} className="day-label font-bold text-center">
+              {day}
             </div>
-          );
-        })}
+          ))}
+          {Array.from({ length: startingDayIndex }).map((_, index) => (
+            <div key={`empty-${index}`} className="empty-day border text-center" />
+          ))}
+          {daysInMonth.map((day, index) => {
+            const formattedDate = ConvertDate(day);
+            const templateName = dateSessionMap[formattedDate];
+            return (
+              <div key={index} className={`day-cell border text-center ${isToday(day) ? "today-cell" : ""}`}>
+                {format(day, "d")}
+                {templateName && (
+                  <div
+                    onClick={() => handleSessionSelection(templateName.record_id)}
+                    className="event-cell border bg-secondary text-light"
+                  >
+                    {templateName.template_name}
+                  </div>
+                )}
+                {!templateName && <div>{templateName ? templateName.template_name : "\u00A0"}</div>}
+              </div>
+            );
+          })}
+        </div>
       </div>
-    </div>
+    </React.Fragment>
   );
 };
 

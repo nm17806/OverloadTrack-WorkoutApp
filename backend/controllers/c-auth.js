7a2c7cd6be277a5cbbb1bade42c5d8e78318cdcb
Nick McCauley <busted_106@hotmail.com>
2024-01-05 10:51:03 +1100

    add some standard workouts for a new user

--- backend/controllers/c-auth.js
+++ backend/controllers/c-auth.js
@@ -25,6 +25,102 @@ const register = async (req, res) => {
       const hash = bcrypt.hashSync(password, salt);
 
       const [result] = await pool.query(`INSERT INTO user_auth (email, password) VALUES (?,?)`, [email, hash]);
+      const id = result.insertId;
+      const [newUserExercises] = await pool.query(
+        `
+      INSERT INTO exercise (exercise_name, body_part, is_active, user_id) VALUES 
+      ('Bench press', 'Chest', '1', ?),
+      ('Bench Fly', 'Chest', '1', ?),
+      ('Lateral Row', 'Back', '1', ?),
+      ('Pull Ups', 'Back', '1', ?),
+      ('Squat', 'Quads', '1', ?),
+      ('RDLs', 'Hamstrings', '1', ?),
+      ('Leg Extensions', 'Quads', '1', ?),
+      ('Leg Curls', 'Hamstrings', '1', ?),
+      ('Dumbbell Shoulder Press', 'Shoulders', '1', ?),
+      ('Lateral Pulldown', 'Back', '1', ?),
+      ('Bicep Curl', 'Biceps', '1', ?),
+      ('Incline Bench Press', 'Chest', '1', ?),
+      ('Cable Rear Delt fly', 'Rear Delts', '1', ?),
+      ('Dumbell Lateral Raises', 'Shoulders', '1', ?),
+      ('Walking Lunges', 'Quads', '1', ?),
+      ('Cable Overhead Pull', 'Triceps', '1', ?),
+      ('Face Pull', 'Back', '1', ?),
+      ('Weighted crunch (cable)', 'Abdominals', '1', ?),
+      ('Hanging Leg Raise', 'Abdominals', '1', ?)
+      `,
+        [id, id, id, id, id, id, id, id, id, id, id, id, id, id, id, id, id, id, id]
+      );
+      const [newUserWorkouts] = await pool.query(
+        `INSERT INTO workout_template (template_name, is_active, user_id) VALUES
+      ('Upper Body', '1', ?),
+      ('Lower Body', '1', ?)
+      `,
+        [id, id]
+      );
+      console.log(newUserExercises);
+      const exerciseId = newUserExercises.insertId;
+      const workoutsId = newUserWorkouts.insertId;
+      const [addExercisesToWorkouts] = await pool.query(
+        `
+      INSERT INTO workout_template_exercise (template_id, exercise_id, is_active, user_id) VALUES
+      (?, ?, '1', ?),
+      (?, ?, '1', ?),
+    (?, ?, '1', ?),
+    (?, ?, '1', ?),
+    (?, ?, '1', ?),
+    (?, ?, '1', ?),
+    (?, ?, '1', ?),
+    (?, ?, '1', ?),
+    (?, ?, '1', ?),
+    (?, ?, '1', ?),
+    (?, ?, '1', ?),
+    (?, ?, '1', ?),
+    (?, ?, '1', ?)
+ 
+      `,
+        [
+          workoutsId,
+          exerciseId,
+          id,
+          workoutsId,
+          exerciseId + 1,
+          id,
+          workoutsId,
+          exerciseId + 2,
+          id,
+          workoutsId,
+          exerciseId + 9,
+          id,
+          workoutsId,
+          exerciseId + 8,
+          id,
+          workoutsId,
+          exerciseId + 13,
+          id,
+          workoutsId,
+          exerciseId + 10,
+          id,
+          workoutsId,
+          exerciseId + 15,
+          id,
+          workoutsId + 1,
+          exerciseId + 4,
+          id,
+          workoutsId + 1,
+          exerciseId + 5,
+          id,
+          workoutsId + 1,
+          exerciseId + 6,
+          id,
+          workoutsId + 1,
+          exerciseId + 7,
+          id,
+          workoutsId + 1,
+          exerciseId + 14,
+          id,
+        ]
+      );
       res.status(201).send({ id: result.insertId, email });
     } catch (err) {
       res.status(500).send({ error: err.message });

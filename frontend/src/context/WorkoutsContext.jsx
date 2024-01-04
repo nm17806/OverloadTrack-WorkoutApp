52837e51a6bdd28b1c8bfa117b1a47662d087889
Nick McCauley <busted_106@hotmail.com>
2024-01-04 16:32:17 +1100

    workoutsContextSetup

--- frontend/src/context/WorkoutsContext.jsx
+++ frontend/src/context/WorkoutsContext.jsx
@@ -1,62 +1,33 @@
-import axios from "axios";
-import { createContext, useState } from "react";
-import { useContext } from "react";
-import { AuthContext } from "./AuthContext";
+import { createContext, useReducer } from "react";
 
 export const WorkoutsContext = createContext();
 
+const workoutsReducer = ({ state, action }) => {
+  switch (action.type) {
+    case "FETCH_WORKOUTS":
+      return {
+        workouts: action.payload,
+      };
+    case "FETCH_WORKOUT_EXERCISES":
+      return {
+        workoutExercises: action.payload,
+      };
+    case "DELETE_EXERCISE":
+      return {
+        workouts: state.workouts.filter((id) => id !== action.payload),
+      };
+    case "DELETE_WORKOUT":
+      return {
+        workouts: state.workouts.filter((id) => id !== action.payload),
+      };
+    default:
+      return state;
+  }
+};
 export const WorkoutsContextProvider = ({ children }) => {
-  const [workouts, setWorkouts] = useState([]);
-  const [workoutName, setWorkoutName] = useState([]);
-
-  const { currentUser } = useContext(AuthContext);
-
-  const fetchWorkouts = async () => {
-    const res = await axios.get("api/workouts", {
-      headers: {
-        Authorization: `Bearer ${currentUser.token}`,
-      },
-    });
-    setWorkoutName(res.data);
-  };
-
-  const fetchWorkoutExercises = async () => {
-    const res = await axios.get(`api/workouts/exercises`, {
-      headers: {
-        Authorization: `Bearer ${currentUser.token}`,
-      },
-    });
-    setWorkouts(res.data);
-  };
-
-  const deleteExercise = async (exerciseId) => {
-    axios.patch(`api/workouts/exercises/${exerciseId}`, null, {
-      headers: {
-        Authorization: `Bearer ${currentUser.token}`,
-      },
-    });
-  };
-
-  const deleteWorkout = async (workoutId) => {
-    axios.patch(`api/workouts/${workoutId}`, null, {
-      headers: {
-        Authorization: `Bearer ${currentUser.token}`,
-      },
-    });
-  };
-
-  return (
-    <WorkoutsContext.Provider
-      value={{
-        workoutName,
-        workouts,
-        fetchWorkouts,
-        fetchWorkoutExercises,
-        deleteExercise,
-        deleteWorkout,
-      }}
-    >
-      {children}
-    </WorkoutsContext.Provider>
-  );
+  const [state, dispatch] = useReducer(workoutsReducer, {
+    workouts: [],
+    workoutExercises: [],
+  });
+  return <WorkoutsContext.Provider value={{ ...state, dispatch }}>{children}</WorkoutsContext.Provider>;
 };
